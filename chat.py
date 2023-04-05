742104ae89c0e4029794a62ee6c9d9fa31723ff6
keiayoun <94325099+keiayoun@users.noreply.github.com>
2023-04-05 00:03:22 +0000

    poo poo

--- chat.py
+++ chat.py
@@ -1,1 +1,35 @@
-https://prod.liveshare.vsengsaas.visualstudio.com/join?84F074DD28174ACFCE7365771E69E9951EE9
+import sys
+import socket
+from chat.client import Client
+from thread import *
+
+def main():
+    # program start
+    print(sys.version)
+    print("program start")
+
+    # initialize socket and port
+    host = "127.0.0.1"
+    port = "8080"
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+
+    # while loop
+        # listen for new clients
+        # new clients will then go to worker threads
+    while 1:
+        s.listen()
+        conn, addr = s.accept()
+        worker_thread(conn, addr)
+
+def worker_thread(conn, addr):
+    while 1:
+        # wait for msg
+        msg = "hello"
+        # if msg is exit, close this connection and exit
+        if msg == "hello":
+            break
+        # send msg
+        print(msg)
+
+main()
---
+++ client.py
@@ -0,0 +1,5 @@
+# functionality
+class Client:
+    def __init__(self, name, port):
+        self.name = name
+        self.port = port
