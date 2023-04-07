73dcdfa2fe297fe1668423b615969ed3a43821b3
keiayoun <94325099+keiayoun@users.noreply.github.com>
2023-04-07 00:09:39 +0000

    need to implement name functionality

--- chat.py
+++ chat.py
@@ -2,7 +2,7 @@ import sys
 import socket
 import threading
 
-https://prod.liveshare.vsengsaas.visualstudio.com/join?26ABF7BBC7248B8173C525910C98D20ACDDB
+threads = []
 
 def main():
     # program start
@@ -26,6 +26,7 @@ def main():
         sock.listen()
         connection, address = sock.accept()
         print("found new client!!")
+        threads.append(connection)
         client = threading.Thread(target=worker_thread, args=(connection, address))
         client.start()
 
@@ -36,20 +37,36 @@ def worker_thread(conn, addr):
     while 1:
         # wait for msg
         msg = conn.recv(2048)
+        msg = msg.decode()
 
         # prints client's msg to server side
-        print("<" + addr[0] + ">" + msg)
+        msg_self = "<you>" + msg
+        msg_other = "<" + addr[0] + ">" + msg
+        print(msg)
 
         # if msg is exit, close this connection and exit
         if msg == "exit":
             break
 
         # send msg to client
-        conn.send(msg)
+        broadcast_msg(msg_self, msg_other, conn)
 
     # prints to server and client that user has left
-    print("<" + addr[0] + " has left>")
-    conn.send("<" + addr[0] + " has left>")
+    msg_other = "<" + addr[0] + " has left>"
+    msg_self = "<You have left>"
+    print(msg_other)
+    broadcast_msg(msg_self, msg_other, conn)
+    threads.remove(conn)
     conn.close()
+
+
+def broadcast_msg(msg_self, msg_other, conn):
+    msg_self = msg_self.encode()
+    msg_other = msg_other.encode()
+    for client in threads:
+        if client != conn:
+            client.send(msg_other)
+        else:
+            client.send(msg_self)
 
 main()
--- client.py
+++ client.py
@@ -1,21 +1,28 @@
-# import sys
-# import socket
+import sys
+import socket
+import select
 
-class Client:
-    def __init__(self, name, port):
-        self.name = name
-        self.port = port
+def main():
+    if len(sys.argv) != 2:
+        print("correct usage: script, name")
+        exit()
 
-# def main():
-#     if len(sys.argv) != 2:
-#         print("correct usage: script, name")
-#         exit()
+    host = "127.0.0.1"
+    port = 8080
+    name = str(sys.argv[1])
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((host, port))
 
-#     host = "127.0.0.1"
-#     port = 8080
-#     name = str(sys.argv[1])
+    while 1:
+        sockets_list = [sys.stdin, server]
+        read_sockets,_, _ = select.select(sockets_list,[],[])
 
-#     while 1:
-#         # yes
+        for socks in read_sockets:
+            if socks == server:
+                msg = socks.recv(2048)
+                print(msg.decode())
+            else:
+                msg = sys.stdin.readline()
+                server.send(msg.encode())
 
-# main()
+main()
