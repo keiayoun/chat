29a713a5ff4059e4105ba00380f94e353f63ac2a
keiayoun <94325099+keiayoun@users.noreply.github.com>
2023-04-12 00:01:34 +0000

    minor qol update

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import sys
 import socket
 import select
+import os
 
 def main():
     if len(sys.argv) != 1:
@@ -15,17 +16,22 @@ def main():
     print("What is your name?")
     name = sys.stdin.readline()
     server.send(name.encode())
+    os.system('clear')
 
     while 1:
         sockets_list = [sys.stdin, server]
         read_sockets,_, _ = select.select(sockets_list,[],[])
-
-        for socks in read_sockets:
-            if socks == server:
-                msg = socks.recv(2048)
-                print(msg.decode())
-            else:
-                msg = sys.stdin.readline()
-                server.send(msg.encode())
+        try:
+            for socks in read_sockets:
+                if socks == server:
+                    msg = socks.recv(2048)
+                    print(msg.decode())
+                else:
+                    msg = sys.stdin.readline()
+                    server.send(msg.encode())
+        except:
+            os.system('clear')
+            print("<chat closed>")
+            break
 
 main()
